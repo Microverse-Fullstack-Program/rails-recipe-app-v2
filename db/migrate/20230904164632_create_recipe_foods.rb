b296bb7a215b7e485893c9c4c15d5ecb6de79878
cherelemma <chere.lemma@aastu.edu.et>
2023-09-04 20:40:07 +0300

    update tables associations

--- db/migrate/20230904164632_create_recipe_foods.rb
+++ db/migrate/20230904164632_create_recipe_foods.rb
@@ -2,8 +2,8 @@ class CreateRecipeFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :recipe_foods do |t|
       t.integer :quantity
-      t.references :Recipe, null: false, foreign_key: true
-      t.references :Food, null: false, foreign_key: true
+      t.references :recipes, null: false, foreign_key: true
+      t.references :foods, null: false, foreign_key: true
 
       t.timestamps
     end
