c89189ab5c0c6db4e0e7942d11e19dd40c787f54
cherelemma <chere.lemma@aastu.edu.et>
2023-09-06 00:50:02 +0300

    Create custome devise error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :inventories
   has_many :recipes
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :password_confirmation, presence: true
 end
