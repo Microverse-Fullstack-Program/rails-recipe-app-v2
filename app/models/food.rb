4256a75475ed04430bdce9a4c901e48641092863
Odongo006 <henryomondi6612@gmail.com>
2023-09-05 19:45:38 +0300

    Add food controller

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,4 +3,8 @@ class Food < ApplicationRecord
   has_many :recipes, through: :recipe_foods
   has_many :inventory_foods, dependent: :destroy
   has_many :inventories, through: :inventory_foods
+
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :measurement_unit, presence: true
+  validates :name, presence: true
 end
