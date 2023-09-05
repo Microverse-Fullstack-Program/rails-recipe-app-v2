3c0cc57a659ff5dbfa23b4316dce08896c079350
cherelemma <chere.lemma@aastu.edu.et>
2023-09-06 01:00:42 +0300

    validate Inventory model

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -2,4 +2,8 @@ class Inventory < ApplicationRecord
   belongs_to :User
   has_many :inventory_foods, dependent: :destroy
   has_many :foods, through: :inventory_foods
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :user_id, presence: true
+  validates :description, presence: true, length: { minimum: 3, maximum: 250 }
 end
