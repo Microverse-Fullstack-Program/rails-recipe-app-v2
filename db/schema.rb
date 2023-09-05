0ea38810fb847ae2dfaa32d6c2b958a10c4519d8
cherelemma <chere.lemma@aastu.edu.et>
2023-09-06 02:28:26 +0300

    update migration and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_05_184641) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_05_064449) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,10 +24,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_05_184641) do
 
   create_table "inventories", force: :cascade do |t|
     t.string "name"
+    t.text "description"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "description"
     t.index ["user_id"], name: "index_inventories_on_user_id"
   end
 
