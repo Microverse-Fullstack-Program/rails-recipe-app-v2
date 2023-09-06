d6685f374ad1b93205627541cd14384eea17fb18
Enning94 <enningfestus@gmail.com>
2023-09-06 19:26:30 +0000

    update migration and schema

--- db/schema.rb
+++ db/schema.rb
@@ -42,12 +42,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_05_064449) do
 
   create_table "recipe_foods", force: :cascade do |t|
     t.integer "quantity"
-    t.bigint "recipes_id", null: false
-    t.bigint "foods_id", null: false
+    t.bigint "recipe_id", null: false
+    t.bigint "food_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["foods_id"], name: "index_recipe_foods_on_foods_id"
-    t.index ["recipes_id"], name: "index_recipe_foods_on_recipes_id"
+    t.index ["food_id"], name: "index_recipe_foods_on_food_id"
+    t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -82,7 +82,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_05_064449) do
   add_foreign_key "inventories", "users"
   add_foreign_key "inventory_foods", "foods"
   add_foreign_key "inventory_foods", "inventories"
-  add_foreign_key "recipe_foods", "foods", column: "foods_id"
-  add_foreign_key "recipe_foods", "recipes", column: "recipes_id"
+  add_foreign_key "recipe_foods", "foods"
+  add_foreign_key "recipe_foods", "recipes"
   add_foreign_key "recipes", "users"
 end
