4c6fbe85b9fddfb1f956d432e4707b2cf548f7a6
cherelemma <chere.lemma@aastu.edu.et>
2023-09-06 17:22:06 +0300

    Update schema and make the recipe public by default

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_05_064449) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_06_141952) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,7 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_05_064449) do
     t.integer "preparation_time"
     t.integer "cooking_time"
     t.text "description"
-    t.boolean "public"
+    t.boolean "public", default: true
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
