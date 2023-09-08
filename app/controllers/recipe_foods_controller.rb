824f6668657924252a35074c236c626f36033d10
Enning94 <enningfestus@gmail.com>
2023-09-08 11:30:04 +0000

    add remove/modify functionalities

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -19,6 +19,40 @@ class RecipeFoodsController < ApplicationController
     end
   end
 
+def edit
+  @recipe_food = RecipeFood.find(params[:id])
+end
+
+  def update
+    @recipe_food = RecipeFood.find(params[:id])
+    id = @recipe_food.recipe_id
+    respond_to do |format|
+        if @recipe_food.update(add_ingredients_params)
+          format.html { redirect_to recipe_path(id), notice: 'Food was successfully updated.' }
+          format.json { render :show, status: :ok, location: @recipe_food }
+        else
+          format.html { render :edit }
+          format.json { render json: @recipe_food.errors, status: :unprocessable_entity }
+        end
+    end
+  end
+  
+    def destroy
+      @recipe_food = RecipeFood.find(params[:id])
+      id = @recipe_food.recipe_id
+  
+      respond_to do |format|
+  
+         if @recipe_food.destroy
+            format.html { redirect_to recipe_path(id), notice: 'Food was successfully removed.' }
+            format.json { render :show, status: :ok, location: @recipe_food }
+        else
+          format.html { redirect_to recipe_path(id), alert: 'You are not authorized to remove this food.' }
+          format.json { render json: @recipe_food.errors, status: :unprocessable_entity }   
+        end
+      end
+    end
+
   def add_ingredients_params
     params.require(:recipe_food).permit(:food_id, :quantity)
   end
