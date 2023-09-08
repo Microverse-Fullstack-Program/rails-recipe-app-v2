31c933ec70e02da6c74d64ac8dd21181896074fd
cherelemma <chere.lemma@aastu.edu.et>
2023-09-08 09:42:14 +0300

    fix linter errors in controller

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,7 +1,9 @@
 class PublicRecipesController < ApplicationController
   def index
     @public_recipes = User.joins(:recipes).joins(:recipe_foods).joins(:foods)
-      .select('users.id AS user_id, users.name AS user_name, recipes.id AS recipe_id, recipes.name AS recipe_name, recipes.description AS recipe_description, SUM(foods.price) AS recipe_price, COUNT(foods.id) AS recipe_quantity')
+      .select('users.id AS user_id, users.name AS user_name, recipes.id AS recipe_id, 
+              recipes.name AS recipe_name, recipes.description AS recipe_description,
+              SUM(foods.price) AS recipe_price, COUNT(foods.id) AS recipe_quantity')
       .group('users.id, recipes.id')
       .where(public: true).order(created_at: :desc)
   end
