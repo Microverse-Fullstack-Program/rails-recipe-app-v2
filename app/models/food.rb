860afa431aef923f8566cafed97d830d3b5ed9c7
cherelemma <chere.lemma@aastu.edu.et>
2023-09-08 09:34:56 +0300

    modify models validation

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,7 +4,7 @@ class Food < ApplicationRecord
   has_many :recipe_foods
   has_many :recipes, through: :recipe_foods
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 50 }
   validates :measurement_unit, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
 end
