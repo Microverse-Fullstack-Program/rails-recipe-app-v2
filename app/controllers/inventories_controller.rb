8966f03c3cea5a0d607e54eb4ab53b49e06a823d
cherelemma <chere.lemma@aastu.edu.et>
2023-09-08 09:49:05 +0300

    remove whitespace

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -10,7 +10,8 @@ class InventoriesController < ApplicationController
 
   def show
     @inventory = Inventory.find(params[:id])
-    @foods = Food.joins(:inventory_foods).where(inventory_foods: { inventory_id: @inventory.id }).select('foods.*, inventory_foods.*')
+    @foods = Food.joins(:inventory_foods).where(inventory_foods: { inventory_id: @inventory.id })
+      .select('foods.*, inventory_foods.*')
   end
 
   def new
