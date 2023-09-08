98c0d188fb3e2b0fd07c37ec05d1057b49bb2c51
Odongo006 <henryomondi6612@gmail.com>
2023-09-08 20:18:04 +0300

    solve public recipe error in controller

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,10 +1,9 @@
 class PublicRecipesController < ApplicationController
   def index
-    @public_recipes = User.joins(:recipes).joins(:recipe_foods).joins(:foods)
-      .select('users.id AS user_id, users.name AS user_name, recipes.id AS recipe_id,
-              recipes.name AS recipe_name, recipes.description AS recipe_description,
-              SUM(foods.price) AS recipe_price, COUNT(foods.id) AS recipe_quantity')
-      .group('users.id, recipes.id')
+    @public_recipes = Recipe.select('recipes.*')
+      .joins(:recipe_foods).joins(:foods)
+      .select('SUM(foods.price) AS price, COUNT(foods.id) AS quantity')
+      .group('recipes.id')
       .where(public: true).order(created_at: :desc)
   end
 
